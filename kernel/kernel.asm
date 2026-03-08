82de04ce1191f4648cab3917fb7689f3712e0a90
Aj Pratham <pratham.aj.26@gmail.com>
2026-03-08 15:36:15 +0530

    Update kernel.asm
    Signed-off-by: Aj Pratham <pratham.aj.26@gmail.com>

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -1,20 +1,11 @@
-; ==================================
-; kernel.asm - Low-level kernel glue
-; ==================================
-
 BITS 32
 
 SECTION .text
-    global kernel_entry
-    extern kernel_main
+global kernel_entry
+extern kernel_main
 
 kernel_entry:
-    ; At this point:
-    ; eax = multiboot magic
-    ; ebx = multiboot info pointer
 
-    push eax
-    push ebx
     call kernel_main
 
 halt:
